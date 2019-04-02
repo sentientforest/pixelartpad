e6c917d8ce91fabe3ce36360786d1e8c2721985a
Michael Graham <oakenclast@gmail.com>
2019-04-02 09:18:18 -0700

    work in progress

--- src/App.js
+++ src/App.js
@@ -9,6 +9,13 @@ class App extends Component {
   constructor(props) {
     super(props)
     this.acceptColor = this.acceptColor.bind(this)
+    this.editPalette = this.editPalette.bind(this)
+    this.state = {
+      palettes: {
+        one: false,
+        two: false
+      }
+    }
   }
 
   acceptColor(arg1, arg2) {
@@ -17,25 +24,54 @@ class App extends Component {
     console.log(arg2)
   }
 
+  editPalette() {
+    let palettes = {...this.state.palettes}
+    palettes.one = !palettes.one
+    this.setState({
+      palettes: palettes
+    })
+  }
+
   render() {
+    let editPalette = this.editPalette 
     return (
       <div className="App">
         <header className="App-header">
           <h1>pixelartpad</h1>
         </header>
-        <div>
-          <div className="drawing-board">
-            <Sheet width={16} height={16}></Sheet>
+        <section class="section">
+          <div className="container">
+            <div className="media">
+              <div className="media-left">
+                <div className="palette-board">
+                  <button className="" onClick={editPalette}>Edit</button>
+                  <Palette />
+                </div>
+              </div>
+              <div className="media-content">
+                <div className="drawing-board">
+                  <Sheet width={16} height={16}></Sheet>
+                </div>
+              </div>
+
+              <div>
+
+              </div>
+            </div>
+            <div className="media">
+              <div className="media-left">
+                <div className="palette-board">
+                  <Palette />
+                </div>
+              </div>
+              <div className="media-content">
+                <div className="drawing-board">
+                  <Sheet width={64} height={64}></Sheet>
+                </div>
+              </div>
+            </div>
           </div>
-        </div>
-        <div className="palette-board">
-          <Palette />
-        </div>
-        <div>
-          <div className="drawing-board">
-            <Sheet width={64} height={64}></Sheet>
-          </div>
-        </div>
+        </section>
       </div>
     );
   }
